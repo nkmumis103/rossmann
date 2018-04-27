ef3db5097f470d7af989aa2903afcab7b77c2f8e
nkmumis103 <32128845+nkmumis103@users.noreply.github.com>
2018-04-28 00:12:26 +0800

    plot

--- inst/www/js/action.js
+++ inst/www/js/action.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 	$(window).scrollTop("0");
 	var bot=$(document).height()-$(window).height();
-	$(".gotop").on("click", function(){ 
+	$(".gotop").on("click", function(){
 		//$(".gotop").css('display','none');
 		jQuery("html,body").animate({
 			scrollTop:0
 		},1000);
 	});
-	$(".gobottom").on("click", function(){ 
+	$(".gobottom").on("click", function(){
 		$(".gobottom").css('display','none');
 		jQuery("html,body").animate({
 			scrollTop:bot
 		},1000);
 	});
 	$(window).scroll(function(){
-		var ScrollTop=$(this).scrollTop();  
+		var ScrollTop=$(this).scrollTop();
 		//console.log(ScrollTop,$(window).height(),$(document).height());
 		if(ScrollTop==0){
 			$(".gotop").css('display','none');
@@ -28,4 +28,5 @@ $(document).ready(function(){
 	$(".tooltip").tooltipster({
 		side:'bottom'
 	});
+	// $(".historyPlot").html(plot.html);
 });
